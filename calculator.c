df41b6a87271abbbde6ed4978abb961debb7ff10
Jaivardhan713 <84133811+Jaivardhan713@users.noreply.github.com>
2021-05-15 18:08:55 +0530

    Update calculator.c
    exponention

--- calculator.c
+++ calculator.c
@@ -19,6 +19,26 @@ void calcAddition() {
 // 3. Multiplication
 // 4. Division
 // 5. Exponentiation
+#include <stdio.h>
+
+int main()
+{
+       int base, exp;
+    long long result = 1;
+    printf("Enter a base number: ");
+    scanf("%d", &base);
+    printf("Enter an exponent: ");
+    scanf("%d", &exp);
+
+    while (exp != 0) 
+    {
+        result *= base;
+        --exp;
+    }
+    printf("Answer = %lld", result);
+    return 0;
+
+}
 
 // 6. Sine
 void calcSine() {
